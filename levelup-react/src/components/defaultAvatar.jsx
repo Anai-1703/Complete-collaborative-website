d629cd5a0b3c90c28650d4bbb5a4645b4e4fee07
Ana Isabel Navarro Gómez <135156247+Anai-1703@users.noreply.github.com>
2023-08-04 20:00:28 +0200

    Delete defaultAvatar.jsx

--- levelup-react/src/components/defaultAvatar.jsx
+++
@@ -1,7 +0,0 @@
-import defaultAvatarSvg from '../assets/svg/default-avatar.svg';
-
-export function DefaultAvatar({ post }) {
-    const className = post ? 'user-avatar' : 'user-avatar-full';
-
-    return <img className={className} src={defaultAvatarSvg} alt="Avatar" />;
-}
