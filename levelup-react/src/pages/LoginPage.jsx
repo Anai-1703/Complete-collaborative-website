a0725da947d428aeb7dac6ec5438420b41323db6
Anai-1703 <135156247+Anai-1703@users.noreply.github.com>
2023-08-05 13:30:24 +0200

    revisando

--- levelup-react/src/pages/LoginPage.jsx
+++ levelup-react/src/pages/LoginPage.jsx
@@ -5,9 +5,8 @@ import { LoginForm } from "../forms/LoginForm";
 export function LoginPage() {
   return (
     <div className="login-page">
-      <div className="form">
         <LoginForm />
       </div>
-    </div>
+    
   );
 }
