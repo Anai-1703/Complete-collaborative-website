37d5aeed3c65f68e800e916a671a25fb0c221c6f
Depechejuan <depechejuan@gmail.com>
2023-08-24 21:09:29 +0200

    arreglado el error fetch

--- levelup-react/src/services/fetchAPI.js
+++ levelup-react/src/services/fetchAPI.js
@@ -1,8 +1,8 @@
 const host = import.meta.env.VITE_API_HOST;
 
-export async function fetchAPI(path, method = "get", payload, token) {
+export async function fetchAPI(path, method = "get", body, token) {
     const requestInit = {
-        method: method,
+        method,
         headers: {},
     };
 
@@ -11,20 +11,20 @@ export async function fetchAPI(path, method = "get", payload, token) {
     }
 
     // añadido el caso delete
-    if ((method === "get" && payload) || (method === "delete" && payload)) {
-        const query = new URLSearchParams(payload).toString();
+    if ((method === "get" && body) || (method === "delete" && body)) {
+        const query = new URLSearchParams(body).toString();
         path += `?${query}`;
     }
 
-    if (method !== "get" && method !== "delete" && payload && !payload.type) {
+    if (method !== "get" && method !== "delete" && body && !body.type) {
         requestInit.headers["Content-Type"] = "application/json";
-        requestInit.body = JSON.stringify(payload);
+        requestInit.body = JSON.stringify(body);
     }
 
-    if (method !== "get" && method !== "delete" && payload.type) {
+    if (method !== "get" && method !== "delete" && body.type) {
         console.log("Subiendo la foto...");
         const form = new FormData();
-        form.append("photo", payload);
+        form.append("photo", body);
         requestInit.body = form;
     }
 
